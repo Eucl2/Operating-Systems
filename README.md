d37e06255835a9ed8396941378516de06e823616
Eucl2 <2001andrebarbosa@gmail.com>
2024-05-05 03:18:32 +0100

    Parallel tasks, status and FCFS working

--- README.md
+++ README.md
@@ -18,23 +18,26 @@ Mais informações no enunciado do projeto
 
 #### Erros:
 
+* Status não mostra o tempo nas tasks completed. -> acontece porque o tempo está apenas a ser calculado no processo filho
+
 * ./client sem args => segmentation fault ✔️
 * Status vai mostrar o tempo de inicio em vez de mostrar o tempo que demorou a executar.  ✔️ -> Added to struct
 * Os ficheiros dos projetos executados devem ser enviados para uma pasta vinda do input. ✔️ -> pasta de testes: file_saver
 * Quando são enviados 2 pedidos de clientes ao mesmo tempo, o id só é devolvido ao cliente do segundo pedido após o pedido do primeiro cliente terminar ✔️
 * Por vezes o input é recebido de forma desformatada no servidor ✔️
 * Status não é recebido corretamente no cliente, ajustar! ✔️
-* Status das tasks não está a ser atualizado corretamente
+* Status das tasks não está a ser atualizado corretamente ✔️
 * Resposta do status não deve conter a palavra terminal "END". ✔️
 * FCFS é na verdade LIFO. Rever código e atualizar ✔️
 
 
 #### Falta:
 
-* Orchestrator tem que receber os argumentos ✔️ -> parallel tasks e sched_policy não estão a ser usadas
-* Mostrar id da tarefa após enviar o pedido ✔️
 * Pipelines no orchestrator
 * Implementar sjf - comparar resultados com o FCFS
+
+* Orchestrator tem que receber os argumentos ✔️ -> parallel tasks e sched_policy não estão a ser usadas
+* Mostrar id da tarefa após enviar o pedido ✔️
 * Orchestrator nao bloquear quando recebe 2 progs ✔️
 * Orchs nao pode morrer ✔️
 * Enviar stdout para txt com o id da tarefa ✔️
@@ -42,8 +45,9 @@ Mais informações no enunciado do projeto
 
 #### Dúvidas:
 * Se fifo nao existir, devemos criá-lo? ou é desnecessário? -> criar : TODO!
+* Ficheiros temporários devem ir para o tmp? e devem ser apagados no final?
+
 * <s>Localizacao dos tests, alterar ou deixar?</s>
 * <s>Posso usar realloc nas pipelines?</s> -> sim
-* Como fechar o orquestrador quando já não quiser usar mais? será que mandar um pedido do cliente ./client shutdown seria aceitável? -> "é completamente aceitável". -> Implementado ✔️
-* ficheiros temporários devem ir para o tmp? e devem ser apagados no final?
-* Proibido usar printf para mostrar ao cliente? -> Deve ser usado write com a função sprintf em conjunção. -> Feito ✔️
+* <s>Como fechar o orquestrador quando já não quiser usar mais? será que mandar um pedido do cliente ./client shutdown seria aceitável?</s> -> "é completamente aceitável". -> Implementado ✔️
+* <s>Proibido usar printf para mostrar ao cliente?</s> -> Deve ser usado write com a função sprintf em conjunção. -> Feito ✔️
--- proj/src/orchestrator.c
+++ proj/src/orchestrator.c
@@ -14,6 +14,7 @@
 
 typedef struct task {
     int id;
+    pid_t pid; //for waiting tasks.
     char command[256];
     char status[20];  // "waiting", "executing", "completed."
     long exec_time; // Real execution time in milliseconds
@@ -48,6 +49,20 @@ Task* find_next_sjf_task() //func to find the next task when the sched policy is
     return selected;
 }
 
+Task* find_task_by_pid(pid_t pid) 
+{
+    Task *current = head;
+    while (current != NULL) 
+    {
+        if (current->pid == pid) 
+        {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
 
 void setup_pipes() 
 {
@@ -63,6 +78,37 @@ void clean_up(int req_fd, int req_fd_write)
     unlink(REQ_PIPE);
     unlink(RESP_PIPE);
 
+}
+
+void update_task_status(Task *task, const char* status) 
+{
+    printf("Updating status of task %d from %s to %s\n", task->id, task->status, status); // Log de mudança de status
+    strcpy(task->status, status);
+    printf("Updated task %d 's status to: %s\n", task->id, task->status);
+}
+
+void process_task_completion(Task *task)
+{
+    update_task_status(task, "completed");
+    activeTasks--;
+}
+
+void check_child_processes() //after execution...
+{
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) 
+    {
+        if (WIFEXITED(status) || WIFSIGNALED(status)) 
+        {
+            Task *completedTask = find_task_by_pid(pid);
+            
+            if (completedTask != NULL) 
+            {
+                process_task_completion(completedTask);
+            }
+        }
+    }
 }
 
 void log_task(Task *task) 
@@ -136,23 +182,16 @@ void write_status_to_client(int fd)
     write(fd, "END\n", strlen("END\n"));
 }
 
-void update_task_status(Task *task, const char* status) 
-{
-    printf("Updating status of task %d from %s to %s\n", task->id, task->status, status); // Log de mudança de status
-    strcpy(task->status, status);
-    printf("Updated task %d 's status to: %s\n", task->id, task->status);
-}
-
 void execute_task(Task *task, const char *output_folder) 
 {
-    printf("Starting execution of task: %d\n", task->id);
     if (task == NULL) return;
+    printf("Starting execution of task: %d\n", task->id);
+    
     struct timeval end_time;
     
     char output_file_name[64]; //required to send stdout e perror to text file with task id
 
     gettimeofday(&task->start_time, NULL); // Start time
-    update_task_status(task, "executing");
 
     int pid = fork();
     if (pid == 0) 
@@ -180,13 +219,45 @@ void execute_task(Task *task, const char *output_folder)
         gettimeofday(&end_time, NULL); // End time
 
         calculate_and_log_duration(task->start_time, end_time, task);
-
-        update_task_status(task, "completed");
+  
         printf("Task %d completed.\n", task->id);
+        //update_task_status(task, "completed");
     } 
     else 
     {
         perror("Failed to fork");
+    }
+}
+
+void dispatch_waiting_tasks(const char *output_folder, int max_parallel_tasks)
+{ //goal: tasks waiting to be executed
+    Task *current = head;
+
+    while (current != NULL && activeTasks < max_parallel_tasks) 
+    {
+        if (strcmp(current->status, "waiting") == 0) 
+        {
+            if (activeTasks < max_parallel_tasks) 
+            {
+                update_task_status(current, "executing");
+                activeTasks++;
+                int pid = fork();
+                if (pid == 0) 
+                {
+                    execute_task(current, output_folder);
+                    exit(0);
+                } 
+                else if (pid > 0) 
+                {
+                    current->pid = pid;
+                } 
+                else 
+                {
+                    perror("Failed to fork");
+                }
+            }
+        }
+        current = current->next;
     }
 }
 
@@ -215,7 +286,7 @@ void handle_command(char* command, const char *output_folder, int max_parallel_t
             strcat(new_task->command, " ");
             token = strtok(NULL, " ");
         }
-
+        
         strcpy(new_task->status, "waiting");
 
         //LIFO
@@ -250,19 +321,30 @@ void handle_command(char* command, const char *output_folder, int max_parallel_t
         write(resp_fd, response, strlen(response));
         close(resp_fd);
 
-        // Fork a process to execute the task
-        
-        int pid = fork();
-        if (pid == 0) 
-        { 
-            // Child process to execute
-            execute_task(new_task, output_folder);
-            exit(0);
-        } 
-        else if (pid < 0) 
+        if (activeTasks < max_parallel_tasks) 
         {
-            perror("Failed to fork");
+            update_task_status(new_task, "executing");
+            activeTasks++;  // Incrementar o contador de tarefas ativas
+
+            // Fork a process to execute the task
+            int pid = fork();
+            
+            if (pid == 0) 
+            {
+                //child process to execute
+                execute_task(new_task, output_folder);
+                exit(0);  // Garante que o processo filho termine após a execução da tarefa
+            } 
+            else if (pid > 0) 
+            {
+                new_task->pid = pid; //so that we can later access the tasks that...hmmm.... are waiting :)
+            } 
+            else 
+            {
+                perror("Failed to fork");
+            }
         }
+        else printf ("Max parallel tasks reached. WAIT!!!\n");
     } 
     else if (strcmp(token, "status") == 0) 
     {
@@ -305,6 +387,8 @@ void handle_requests(const char *output_folder, const char *sched_policy, int ma
         char command[300];
         while (read(req_fd, command, sizeof(command) - 1) > 0) 
         {
+            check_child_processes(); //no blocking while waiting for child to be completed
+            dispatch_waiting_tasks(output_folder, max_parallel_tasks); //waiting to execute
             printf(">Comando a executar: %s\n", command);
             command[sizeof(command) - 1] = '\0'; // Ensure null-terminated
             handle_command(command, output_folder, max_parallel_tasks);
